b9dee8fac61993df46d47327e0397fa417e2c2c9
assi23 <guiassi@hotmail.com>
2022-04-03 21:46:06 -0300

    aprendendo o conceito de propriedade

--- 01-github-explorer/src/components/RepositoryList.jsx
+++ 01-github-explorer/src/components/RepositoryList.jsx
@@ -1,33 +1,20 @@
-const repositoryName = 'unform2';
+import { RepositoryItem } from "./RepositoryItem";
+
+const repository = {
+    name: 'IgniteReactJs',
+    description: 'Curso ignite trilha ReactJS',
+    link: 'https://github.com/assi23/IgniteReactJs'
+}
 
 export function RepositoryList() {
     return (
         <section className="repository-list">
             <h1> Lista de repositórios</h1>
             <ul>
-                <li>
-                    <strong>{repositoryName}</strong>
-                    <p> Forms in React</p>
-                    <a href="">
-                        Acessar repositório
-                    </a>
-                </li>
-
-                <li>
-                    <strong> unform </strong>
-                    <p> Forms in React</p>
-                    <a href="">
-                        Acessar repositório
-                    </a>
-                </li>
-
-                <li>
-                    <strong> unform </strong>
-                    <p> Forms in React</p>
-                    <a href="">
-                        Acessar repositório
-                    </a>
-                </li>
+                <RepositoryItem repository={repository} />
+                <RepositoryItem repository={repository} />
+                <RepositoryItem repository={repository} />
+                <RepositoryItem repository={repository} />
             </ul>
         </section>
     )
