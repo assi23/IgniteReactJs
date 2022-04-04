9b53035fc4b20635b7b9034b1562582c4085272f
assi23 <guiassi@hotmail.com>
2022-04-04 20:29:30 -0300

    using fast-refresh and configure

--- 01-github-explorer/src/components/Counter.jsx
+++ 01-github-explorer/src/components/Counter.jsx
@@ -10,7 +10,9 @@ export function Counter() {
     return (
         <div>
             <h2>{counter}</h2>
-            <button type="button" onClick={Increment}>Increment</button>
+            <button type="button" onClick={Increment}>
+                Increment
+            </button>
         </div>
     )
 }
--- 01-github-explorer/webpack.config.js
+++ 01-github-explorer/webpack.config.js
@@ -1,12 +1,13 @@
 const path = require('path')
 const htmlWebpackPlugin = require('html-webpack-plugin')
+const reactRefreshWebPackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 
-const isDevelopment = process.env.NODE_ENV !== 'production' 
+const isDevelopment = process.env.NODE_ENV !== 'production'
 
 module.exports = {
     mode: isDevelopment ? 'development' : 'production',
     devtool: isDevelopment ? 'eval-source-map' : 'source-map',
-        entry: path.resolve(__dirname, 'src', 'index.jsx'),
+    entry: path.resolve(__dirname, 'src', 'index.jsx'),
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
@@ -16,25 +17,33 @@ module.exports = {
     },
     devServer: {
         static: {
-            directory: path.join(__dirname, 'public')
+            directory: path.join(__dirname, 'public'),
         },
     },
     plugins: [
+        isDevelopment && new reactRefreshWebPackPlugin(),
         new htmlWebpackPlugin({
             template: path.resolve(__dirname, 'public', 'index.html')
         })
-    ],
+    ].filter(Boolean),
     module: {
         rules: [
             {
                 test: /\.jsx$/,
                 exclude: /node_modules/,
-                use: 'babel-loader',
+                use: {
+                    loader: 'babel-loader',
+                    options:{
+                       plugins: [
+                        isDevelopment && require.resolve('react-refresh/babel')
+                       ].filter(Boolean) 
+                    }
+                },
             },
             {
                 test: /\.scss$/,
                 exclude: /node_modules/,
-                use: ['style-loader','css-loader','sass-loader'],
+                use: ['style-loader', 'css-loader', 'sass-loader'],
             }
         ],
     }
